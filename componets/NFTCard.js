a3dc5e5aaa73e9ecee954ef5868d54ce10b56c4e
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-13 09:18:10 -0700

    initializing nft app

---
+++ componets/NFTCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NFTCard = () => {
+  return (
+    <View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+}
+
+export default NFTCard;
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useState} from 'react';
+import { View, Text, FlatListItem, SafeArea, SafeAreaView } from 'react-native';
+import { COLORS, NFTData} from '../constants'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+       
+    </SafeAreaView>
+  );
+}
+
+export default Home;
