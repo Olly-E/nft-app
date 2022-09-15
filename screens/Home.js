18883a25c2a75129b60997ee63b46c077e12a17b
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-14 23:22:34 -0700

    homepage done

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,36 @@
-import React from 'react';
 import {useState} from 'react';
-import { View, Text, FlatListItem, SafeArea, SafeAreaView } from 'react-native';
-import { COLORS, NFTData} from '../constants'
+import { View, Text, FlatListItem, SafeArea, FlatList} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { COLORS, NFTData} from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../componets';
 
 const Home = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-       
+       <FocusedStatusBar background={COLORS.primary}/>
+
+       <View style={{flex: 1}}>
+          <View style={{zIndex: 0}}>
+            <FlatList
+              data={NFTData}
+              renderItem={({item}) => <NFTCard data={item}/>}
+              keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={false}
+              ListHeaderComponent={<HomeHeader />}
+            />
+          </View>
+          <View style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}>
+            <View style={{height: 300, backgroundColor: COLORS.primary}}/>
+            <View style={{flex: 1, backgroundColor: COLORS.white}}/>
+          </View>
+       </View>
     </SafeAreaView>
   );
 }
